d573c891d69e251b37283e016f0be3f0743b276a
NafisRohan12 <nafisislamrohan@gmail.com>
2026-09-13 23:15:03 +0600

    chore: initial project setup with Spring Web and Spring Security

---
+++ src/main/java/io/github/nafisrohan/auth_spring_boot_starter/AuthSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.nafisrohan.auth_spring_boot_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nafisrohan/auth_spring_boot_starter/AuthSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nafisrohan.auth_spring_boot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
